c3bc440229c1dc476fa7e7c5b661a89e7c584537
Andrew Gubin <thoughcrayfish@gmail.com>
2016-09-25 20:33:41 +0600

    - implemented tutorial, small bug fixes

---
+++ app/src/main/java/com/example/musicplayerapp/ui/AbstractFragmentActivity.java
@@ -0,0 +1,9 @@
+package com.example.musicplayerapp.ui;
+
+/**
+ * Created by Crayfish on 24/09/16.
+ */
+
+public class AbstractFragmentActivity
+{
+}
---
+++ app/src/main/java/com/example/musicplayerapp/ui/tutorial/IntroActivity.java
@@ -0,0 +1,9 @@
+package com.example.musicplayerapp.ui.tutorial;
+
+/**
+ * Created by Crayfish on 24/09/16.
+ */
+
+public class IntroActivity
+{
+}
